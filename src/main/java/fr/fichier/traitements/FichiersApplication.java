530eb2d4d340f3ca4efa4b451fb5df9c24d88fda
BOZI <yao-git@gmx.fr>
2024-02-27 18:36:24 +0100

    Boot Fichier

---
+++ src/main/java/fr/fichier/traitements/FichiersApplication.java
@@ -0,0 +1,13 @@
+package fr.fichier.traitements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichiersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichiersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/fichier/traitements/FichiersApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fichier.traitements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichiersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
